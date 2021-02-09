8be5d2b5bf1df7a9c24cbb6e1b3a77bba75dc98f
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-02-09 18:16:02 -0300

    feat(build): Base of the project structure in SwiftUI

---
+++ LinguaDeLadeira/LinguaDeLadeira/LinguaDeLadeiraApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinguaDeLadeiraApp.swift
+//  LinguaDeLadeira
+//
+//  Created by Juliano Vaz on 09/02/21.
+//
+
+import SwiftUI
+
+@main
+struct LinguaDeLadeiraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
