dd1a7a448246819c32ded10ef885059e65494f35
Juliano Cezar Teles Vaz <julianoctvaz@gmail.com>
2021-02-10 23:49:03 -0300

    feat(UI): Creates InicioView

--- LinguaDeLadeira/LinguaDeLadeira/Telas/InicioView.swift
+++ LinguaDeLadeira/LinguaDeLadeira/Telas/InicioView.swift
@@ -9,7 +9,44 @@ import SwiftUI
 
 struct InicioView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack{
+            Image("linguaDeLadeiraTitulo")
+                .resizable()
+                .aspectRatio(contentMode:.fit)
+                .frame(width: UIScreen.main.bounds.width*1.1, height: UIScreen.main.bounds.height*0.5, alignment: .center)
+            
+            Spacer()
+            
+           
+            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/){
+                Button(
+                    action: {
+                        //acao da navegacao
+                    },
+                    label: {
+                        Image("botaoIniciar")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: UIScreen.main.bounds.width*0.4, height:  UIScreen.main.bounds.height*0.1, alignment: .center)
+                    }
+                )
+     
+                Button(
+                    action: {
+                        //acacao para ir para o onboarding de novo
+                    },
+                    label: {
+                        Image("botaoComoJogar")
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: UIScreen.main.bounds.width*0.4, height:  UIScreen.main.bounds.height*0.1, alignment: .center)
+                    }
+                )
+            }
+            
+            Spacer()
+               
+        }
     }
 }
 
@@ -17,6 +54,6 @@ struct InicioView_Previews: PreviewProvider {
     static var previews: some View {
         InicioView()
             .previewLayout(PreviewLayout.fixed(width: 896, height: 414))
-//            .previewDevice("iPhone 11") esse ai é tamanho do iphone 11, se for ver em outros devices coloca outros tamanhos
+//            .previewDevice("iPhone 11").previewLayout(PreviewLayout.fixed(width: 896, height: 414))  //esse ai é tamanho do iphone 11, se for ver em outros devices coloca outros tamanhos
     }
 }
